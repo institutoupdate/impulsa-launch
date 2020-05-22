abdb5a9255b21775a1e125ffdb4ae607c3a1acc9
Miguel Peixe <miguel@peixe.co>
2020-05-22 13:40:59 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "styles/main.less";
+
+export default class App extends React.Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+const ENV = process.env.NODE_ENV || "development";
+
+let entry = [path.resolve("src")];
+let plugins = [
+  new webpack.DefinePlugin({
+    "process.env": {
+      NODE_ENV: JSON.stringify(ENV),
+    },
+  }),
+];
+
+const htmlPlugin = new HTMLWebpackPlugin({
+  template: path.resolve(__dirname, "src", "index.html"),
+  filename: "index.html",
+  inject: "body",
+});
+
+if (ENV !== "production") {
+  plugins.unshift(htmlPlugin);
+  plugins = plugins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ]);
+} else {
+  plugins.unshift(htmlPlugin);
+  plugins = plugins.concat([new webpack.NoEmitOnErrorsPlugin()]);
+}
+
+module.exports = {
+  mode: ENV,
+  devtool: "#source-map",
+  entry,
+  resolve: {
+    alias: {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+    },
+    modules: ["src", "node_modules"],
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/",
+    filename: function () {
+      if (ENV == "production") {
+        return "[name]-[hash].js";
+      } else {
+        return "[name].js";
+      }
+    },
+  },
+  plugins,
+  module: {
+    rules: [
+      {
+        test: /\.js(x)?$/,
+        use: {
+          loader: "babel-loader",
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|less)$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /fonts\/(.*)\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: "file-loader?name=fonts/[name].[hash].[ext]&publicPath=/",
+      },
+      {
+        test: /\.(png|jpg|svg|gif|mp4)$/,
+        use: "file-loader?name=assets/[name].[hash].[ext]&publicPath=/",
+      },
+    ],
+  },
+};
